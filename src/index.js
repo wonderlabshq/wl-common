390b09cf70634f0082d0541a7f118f44b82a3706
hanifali <hanif@wonderlabs.io>
2017-02-11 17:38:52 +0700

    create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const common = {};
+module.exports = common;
+
+common.config = require('./config');
+common.container = require('./container');
+common.errors = require('./errors');
+common.utils = require('./utils');
